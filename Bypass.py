03fcd939a4d76ede781168942e4c277abcc2fbcb
Riaz404 <104278956+Riaz404@users.noreply.github.com>
2022-04-24 02:11:46 +0500

    Create Bypass.py
    apt update
    apt upgrade
    apt install git
    apt install python
    apt install python2
    pip2 install requests
    pip2 install mechanize
    git clone https://github.com/Riaz404/Bypass.git
    ls
    cd Bypass
    ls
    python2 Bypass.py

---
+++ Bypass.py
@@ -0,0 +1,76 @@
+#non Recode
+#code by MR.FAGHP
+import os
+from time import sleep
+
+os.system('clear')
+
+sleep(3)
+print '          1 select karke age run karo          '
+print '========================================='
+print '|(1). SSB. QAISER. MR.ADI . AND FILE MAKING |'
+print '|(2). Exit                              |'
+print '========================================='
+print '|Author         : RIAZKHAN/ZULFAQAR ALI |'
+print '|TOOL STATUS : RK/ZK BYPASS ______  |'
+print '========================================'
+
+pilih = input('====> ')
+
+if pilih == 1:
+     print '                yaha se select karo                '
+     print '============================================'
+     print '(2) SSB'
+     print '(3) QAISER '
+     print '(4) RESHU'
+     print '(5) FILE MAKING'
+     print '==========================================='
+
+pilih = input('====> ')
+
+if pilih == 2:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.system('pkg install python')
+     os.system('pkg install python2')
+     os.system('pip2 install requests & mechanize')
+     os.system('pkg install git')
+     os.system('git clone https://github.com/Sarfraz-Ssb/MSB.git')
+     os.system('cd MSB')
+     os.system('python MSB.py')
+
+if pilih == 3:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.systen('pkg install git')
+     os.system('pkg install python && pkg install python2')
+     os.system('pkg install python3')
+     os.system('pip2 install requests')
+     os.system('pip install mechanize')
+     os.system('git clone https://github.com/Qadirking/Mahar.git')
+     os.system('cd Mahar')
+     os.system('python Qadir.py')
+
+if pilih == 4:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.systen('pkg install git')
+     os.system('pkg install python && pkg install python2')
+     os.system('pip2 install bs4')
+     os.system('pip2 install rewuests')
+     os.system('pip install mechanize')
+     os.system('git clone https://github.com/Alon3-Rishu/premium_.git')
+     os.system('cd premium_')
+     os.system('python2 Rishupro py')
+     
+if pilih == 5:
+     print 'Tunggu'
+     os.system('pkg update && pkg upgrade')
+     os.systen('pkg install git')
+     os.system('pkg install python && pkg install python2')
+     os.system('pip2 install bs4')
+     os.system('pip2 install rewuests')
+     os.system('pip install mechanize')
+     os.system('git clone https://github.com/ONU-XD/FILE_MAKING')
+     os.system('cd FILE-MAKING')
+     os.system('python2 FILE_MAKING.py')
